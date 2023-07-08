adb79c98f9a3fd776ec80677846357d495f7a816
JWMatheo <88055282+JWMatheo@users.noreply.github.com>
2023-07-08 21:01:24 +0800

    Update getArrayFromMapping.sol

--- getArrayFromMapping.sol
+++ getArrayFromMapping.sol
@@ -1,7 +1,6 @@
 
-Replace <TYPE> by your type. Exemples of common type are address, uint, string, bool, bytes or struc. Please refer to the official documentation : https://solidity-fr.readthedocs.io/fr/latest/types.html
+// Replace <TYPE> by your type. Exemples of common type are address, uint, string, bool, bytes or struc. Please refer to the official documentation : https://solidity-fr.readthedocs.io/fr/latest/types.html
 
-```solidity
 pragma solidity ^0.8.19;
 mapping(address => <TYPE>[]) public mapToArrayOfAddress;
 
@@ -13,4 +12,3 @@ function getArrayFromMapping(address _address) external view returns(<TYPE>[] me
         }
         return temp;
     }
-```
