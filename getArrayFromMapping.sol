65389508d13a85f0a345295803ad1380819f045e
JWMatheo <88055282+JWMatheo@users.noreply.github.com>
2023-07-08 20:57:12 +0800

    Create getArrayFromMapping.sol

---
+++ getArrayFromMapping.sol
@@ -0,0 +1,13 @@
+
+Replace <TYPE> by your type. Exemples of common type are address, uint, string, bool, bytes or struc. Please refer to the official documentation : https://solidity-fr.readthedocs.io/fr/latest/types.html
+
+mapping(address => <TYPE>[]) public mapToArrayOfAddress;
+
+function getArrayFromMapping(address _address) external view returns(<TYPE>[] memory){
+        <TYPE>[] memory temp = new <TYPE>[](mapToArrayOfAddress[_address].length);
+        for (uint32 i = 0; i < mapToArrayOfAddress[_address].length; i++) 
+        {
+            temp[i] = mapToArrayOfAddress[_address][i];
+        }
+        return temp;
+    }
